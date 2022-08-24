ba737fef61acf23d4f26a0c189df3b2b2480d311
Scorevictim <30682499+scorevictim@users.noreply.github.com>
2022-08-24 00:28:50 -0700

    Updated main app file

--- src/CustomerManager.java
+++ src/CustomerManager.java
@@ -5,8 +5,6 @@ public class CustomerManager {
     public static void main(String[] args) throws Exception {
         CustomerList customerList = new CustomerList(10);
         CustomerView view = new CustomerView(customerList);
-
         view.enterMainMenu();
-        System.out.println("Hello, World!");
     }
 }
